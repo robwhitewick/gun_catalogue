ba48c17d5d5d4212c98cb0d38cc73f74ba54f9be
SteadySpring8 <robertayling927@gmail.com>
2020-06-02 20:14:06 +0100

    inventory config

---
+++ guncatalogue_with_inventory/inventory config.lua
@@ -0,0 +1,64 @@
+copy this into your redemrp_inventory config.
+
+
+Config.Usable = {
+    
+    "22_ammo",
+    "rifle_ammo",
+    "shotgun_ammo",
+    "revolver_ammo",
+    "pistol_ammo",
+    "repeator_ammo",
+    "snipe_ammo",
+    "arrows",
+    "knives",
+}
+
+Config.Labels = {
+    --PISTOLS
+    ["WEAPON_REVOLVER_CATTLEMAN"] = "Cattleman Revolver",
+    ["WEAPON_REVOLVER_DOUBLEACTION"] = "DoubleAction",
+    ["WEAPON_PISTOL_VOLCANIC"] = "Volcanic",
+    ["WEAPON_PISTOL_SEMIAUTO"] = "SemiAuto",
+    ["WEAPON_PISTOL_MAUSER"] = "Mauser",
+    ["WEAPON_REVOLVER_LEMAT"] = "Lemat",
+    ["WEAPON_REVOLVER_SCHOFIELD"] = "Schofield",
+    --REAPEATERS
+    ["WEAPON_REPEATER_CARBINE"] = "Carbine",
+    ["WEAPON_REPEATER_EVANS"] = "Evans",
+    ["WEAPON_REPEATER_HENRY"] = "Henry",
+    ["WEAPON_REPEATER_WINCHESTER"] = "Winchester",
+    --RIFLES
+    ["WEAPON_RIFLE_VARMINT"] = "Varmint",
+    ["WEAPON_RIFLE_BOLTACTION"] = "Bolt action",
+    ["WEAPON_SNIPERRIFLE_CARCANO"] = "Carcano",
+    ["WEAPON_SNIPERRIFLE_ROLLINGBLOCK"] = "Rolling block",
+    ["WEAPON_RIFLE_SPRINGFIELD"] = "Springfield",
+    --SHOTGUNS
+    ["WEAPON_SHOTGUN_PUMP"] = "Pump",
+    ["WEAPON_SHOTGUN_REPEATING"] = "Repeating shotgun",
+    ["WEAPON_SHOTGUN_DOUBLEBARREL"] = "Doublebarrel",
+    ["WEAPON_SHOTGUN_SAWEDOFF"] = "Sawed off",
+    ["WEAPON_SHOTGUN_SEMIAUTO"] = "Semiauto Shotgun",
+    --MELEE WEAPONS
+    ["WEAPON_MELEE_KNIFE"] = "Knife",
+    ["WEAPON_MELEE_LANTERN"] = "Lantern",
+    ["WEAPON_BOW"] = "Bow",
+    ["WEAPON_LASSO"] = "Lasso",
+    ["WEAPON_MELEE_HATCHET"] = "Hatchet",
+    ["WEAPON_THROWN_THROWING_KNIVES"] = "Throwing knives",
+    ["WEAPON_MELEE_MACHETE"] = "Machete",
+    ["WEAPON_THROWN_TOMAHAWK"] = "Tomahawk",
+    ["WEAPON_THROWN_DYNAMITE"] = "Dynamite stick",
+    ["WEAPON_THROWN_MOLOTOV"] = "Molotov",
+    -- ammos
+
+    ["revolver_ammo"] = "Revolver Ammo",
+    ["pistol_ammo"] = "Pistol Ammo",
+    ["22_ammo"] = "22 Ammo",
+    ["rifle_ammo"] = "Rifle Ammo",
+    ["shotgun_ammo"] = "Shotgun Ammo",
+    ["repeator_ammo"] = "Repeator Ammo",
+    ["snipe_ammo"] = "Sniper Ammo",
+    ["arrows"] = "Arrows",
+}
