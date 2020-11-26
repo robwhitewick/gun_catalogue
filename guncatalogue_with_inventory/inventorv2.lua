160e65e7aa830ca938851444b1175cd0c8b591d4
Abdulkadir AKTAŞ <abdulkadir.aktas@hotmail.com>
2020-11-26 12:19:22 +0300

    Create inventorv2.lua

---
+++ guncatalogue_with_inventory/inventorv2.lua
@@ -0,0 +1,408 @@
+copy this into your redemrp_inventory config.
+--PISTOLS
+Config.Items = {
+  ["WEAPON_REVOLVER_CATTLEMAN"] = {
+      label = "Cattleman Revolver",
+      description = "Cattleman Revolver",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REVOLVER_CATTLEMAN"),
+      imgsrc = "items/WEAPON_REVOLVER_CATTLEMAN.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_REVOLVER_DOUBLEACTION"] = {
+      label = "DoubleAction",
+      description = "DoubleAction",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REVOLVER_DOUBLEACTION"),
+      imgsrc = "items/WEAPON_REVOLVER_DOUBLEACTION.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_PISTOL_VOLCANIC"] = {
+      label = "Volcanic",
+      description = "Volcanic",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_PISTOL_VOLCANIC"),
+      imgsrc = "items/WEAPON_PISTOL_VOLCANIC.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_PISTOL_SEMIAUTO"] = {
+      label = "SemiAuto",
+      description = "SemiAuto",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_PISTOL_SEMIAUTO"),
+      imgsrc = "items/WEAPON_PISTOL_SEMIAUTO.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_PISTOL_MAUSER"] ={
+      label = "Mauser",
+      description = "Mauser",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_PISTOL_MAUSER"),
+      imgsrc = "items/WEAPON_PISTOL_MAUSER.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_REVOLVER_LEMAT"] ={
+      label = "Lemat",
+      description = "Lemat",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REVOLVER_LEMAT"),
+      imgsrc = "items/WEAPON_REVOLVER_LEMAT.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_REVOLVER_SCHOFIELD"] = {
+      label = "Schofield",
+      description = "Schofield",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REVOLVER_SCHOFIELD"),
+      imgsrc = "items/WEAPON_REVOLVER_SCHOFIELD.png",
+      type = "item_weapon"
+  },
+  --REAPEATERS
+  ["WEAPON_REPEATER_CARBINE"] = {
+      label = "Carbine",
+      description = "Carbine",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REPEATER_CARBINE"),
+      imgsrc = "items/WEAPON_REPEATER_CARBINE.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_REPEATER_EVANS"] = {
+      label = "Evans",
+      description = "Evans",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REPEATER_EVANS"),
+      imgsrc = "items/WEAPON_REPEATER_EVANS.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_REPEATER_HENRY"] = {
+      label = "Henry",
+      description = "Henry",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REPEATER_HENRY"),
+      imgsrc = "items/WEAPON_REPEATER_HENRY.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_REPEATER_WINCHESTER"] = {
+      label = "Winchester",
+      description = "Winchester",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_REPEATER_WINCHESTER"),
+      imgsrc = "items/WEAPON_REPEATER_WINCHESTER.png",
+      type = "item_weapon"
+  },
+  --RIFLES
+  ["WEAPON_RIFLE_VARMINT"] = {
+      label = "Varmint",
+      description = "Varmint",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_RIFLE_VARMINT"),
+      imgsrc = "items/WEAPON_RIFLE_VARMINT.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_RIFLE_BOLTACTION"] = {
+      label = "Bolt action",
+      description = "Bolt action",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_RIFLE_BOLTACTION"),
+      imgsrc = "items/WEAPON_RIFLE_BOLTACTION.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_SNIPERRIFLE_CARCANO"] = {
+      label = "Carcano",
+      description = "Carcano",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_SNIPERRIFLE_CARCANO"),
+      imgsrc = "items/WEAPON_SNIPERRIFLE_CARCANO.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_SNIPERRIFLE_ROLLINGBLOCK"] = {
+      label = "Rolling block",
+      description = "Rolling block",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_SNIPERRIFLE_ROLLINGBLOCK"),
+      imgsrc = "items/WEAPON_SNIPERRIFLE_ROLLINGBLOCK.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_RIFLE_SPRINGFIELD"] = {
+      label = "Springfield",
+      description = "Springfield",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_RIFLE_SPRINGFIELD"),
+      imgsrc = "items/WEAPON_RIFLE_SPRINGFIELD.png",
+      type = "item_weapon"
+  },
+  --SHOTGUNS
+  ["WEAPON_SHOTGUN_PUMP"] = {
+      label = "Pump",
+      description = "Pump",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_SHOTGUN_PUMP"),
+      imgsrc = "items/WEAPON_SHOTGUN_PUMP.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_SHOTGUN_REPEATING"] = {
+      label = "Repeating shotgun",
+      description = "Repeating shotgun",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_SHOTGUN_REPEATING"),
+      imgsrc = "items/WEAPON_SHOTGUN_REPEATING.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_SHOTGUN_DOUBLEBARREL"] = {
+      label = "Doublebarrel",
+      description = "Doublebarrel",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_SHOTGUN_DOUBLEBARREL"),
+      imgsrc = "items/WEAPON_SHOTGUN_DOUBLEBARREL.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_SHOTGUN_SAWEDOFF"] = {
+      label = "Sawed off",
+      description = "Sawed off",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_SHOTGUN_SAWEDOFF"),
+      imgsrc = "items/WEAPON_SHOTGUN_SAWEDOFF.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_SHOTGUN_SEMIAUTO"] = {
+      label = "Semiauto Shotgun",
+      description = "Semiauto Shotgun",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_SHOTGUN_SEMIAUTO"),
+      imgsrc = "items/WEAPON_SHOTGUN_SEMIAUTO.png",
+      type = "item_weapon"
+  },
+  --MELEE WEAPONS
+  ["WEAPON_MELEE_KNIFE"] = {
+      label = "Knife",
+      description = "Knife",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_MELEE_KNIFE"),
+      imgsrc = "items/WEAPON_MELEE_KNIFE.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_MELEE_LANTERN"] = {
+      label = "Lantern",
+      description = "Lantern",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_MELEE_LANTERN"),
+      imgsrc = "items/WEAPON_MELEE_LANTERN.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_BOW"] = {
+      label = "Bow",
+      description = "Bow",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_BOW"),
+      imgsrc = "items/WEAPON_BOW.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_LASSO"] = {
+      label = "Lasso",
+      description = "Lasso",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_LASSO"),
+      imgsrc = "items/WEAPON_LASSO.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_MELEE_HATCHET"] = {
+      label = "Hatchet",
+      description = "Hatchet",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_MELEE_HATCHET"),
+      imgsrc = "items/WEAPON_MELEE_HATCHET.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_THROWN_THROWING_KNIVES"] = {
+      label = "Throwing knives",
+      description = "Throwing knives",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_THROWN_THROWING_KNIVES"),
+      imgsrc = "items/WEAPON_THROWN_THROWING_KNIVES.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_MELEE_MACHETE"] = {
+      label = "Machete",
+      description = "Machete",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_MELEE_MACHETE"),
+      imgsrc = "items/WEAPON_MELEE_MACHETE.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_THROWN_TOMAHAWK"] = {
+      label = "Tomahawk",
+      description = "Tomahawk",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_THROWN_TOMAHAWK"),
+      imgsrc = "items/WEAPON_THROWN_TOMAHAWK.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_THROWN_DYNAMITE"] = {
+      label = "Dynamite stick",
+      description = "Dynamite stick",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_THROWN_DYNAMITE"),
+      imgsrc = "items/WEAPON_THROWN_DYNAMITE.png",
+      type = "item_weapon"
+  },
+  ["WEAPON_THROWN_MOLOTOV"] = {
+      label = "Molotov",
+      description = "Molotov",
+      weight = 0.9,
+      canBeDropped = true,
+      requireLvl = 1,
+      weaponHash = GetHashKey("WEAPON_THROWN_MOLOTOV"),
+      imgsrc = "items/WEAPON_THROWN_MOLOTOV.png",
+      type = "item_weapon"
+  },
+  -- ammos
+
+  ["revolver_ammo"] = {
+      label = "Revolver Ammo",
+      description = "Revolver Ammo",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/revolver_ammo.png"
+  },
+  ["pistol_ammo"] = {
+      label = "Pistol Ammo",
+      description = "Pistol Ammo",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/pistol_ammo.png"
+  },
+  ["22_ammo"] = {
+      label = "22 Ammo",
+      description = "22 Ammo",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/22_ammo.png"
+  },
+  ["rifle_ammo"] = {
+      label = "Rifle Ammo",
+      description = "Rifle Ammo",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/rifle_ammo.png"
+  },
+  ["shotgun_ammo"] = {
+      label = "Shotgun Ammo",
+      description = "Shotgun Ammo",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/shotgun_ammo.png"
+  },
+  ["repeator_ammo"] = {
+      label = "Repeator Ammo",
+      description = "Repeator Ammo",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/repeator_ammo.png"
+  },
+  ["snipe_ammo"] = {
+      label = "Sniper Ammo",
+      description = "Sniper Ammo",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/snipe_ammo.png"
+  },
+  ["arrows"] = {
+      label = "Arrows",
+      description = "Arrows",
+      weight = 0.02,
+      canBeDropped = true,
+      canBeUsed = true,
+      requireLvl = 0,
+      limit = 64,
+      type = "item_standard",
+      imgsrc = "items/arrows.png"
+  }
+}
